a997080a983d432a44f7fa5acd09c30e2a09cb8c
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-12-01 15:08:05 +0100

    Releasing to PyPI.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 import os
 import setuptools
 from setuptools import setup
-import version
+
+# The version of this library at the time of forking it
+_version = '1.11.0'
 
 requirements = list(open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), 'r').readlines())
 
 print(setuptools.find_packages('src'))
 
-setup(name='python-tds',
-      version=version.get_git_version(),
+setup(name='zato-ext-python-tds',
+      version=_version,
       description='Python DBAPI driver for MSSQL using pure Python TDS (Tabular Data Stream) protocol implementation',
       author='Mikhail Denisenko',
       author_email='denisenkom@gmail.com',
